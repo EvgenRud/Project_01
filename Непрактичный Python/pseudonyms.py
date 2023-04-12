93f52c4a57f3af914c603ca47a9c61103744be7a
EvgenRud <rudnik.evgen@gmail.com>
2023-04-12 21:35:29 +0400

    Заключили программу в функцию main

--- Непрактичный Python/pseudonyms.py
+++ Непрактичный Python/pseudonyms.py
@@ -1,58 +1,65 @@
-# Проект 1. Генерерование псевдонимов.
+# Проект 1. Генерирование псевдонимов.
 
 # Импортируем модуль'random'
 
 import random
 
-print("Welcome to the Psych 'Sidekick Name Picker.'\n")
-print("A name just like Sean would pick for Gus:\n\n")
+# Исключаем код из глобального пространства функцией main()
+def main():
 
-# Определяем кортежи имён
+    print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+    print("A name just like Sean would pick for Gus:\n\n")
 
-first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
-         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
-         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
-         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
-         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
-         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
-         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
-         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
-         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
-         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
-         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
-         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
-         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
-         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
-         "Winston 'Jazz Hands'", 'Worms')
+    # Определяем кортежи имён
 
-last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
-        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
-        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
-        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
-        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
-        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
-        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
-        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
-        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
-        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
-        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
-        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
-        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
-        'Woolysocks')
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+            "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+            'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+            'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+            'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+            'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+            'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+            'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+            '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+            'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+            'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+            "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+            'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+            'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+            "Winston 'Jazz Hands'", 'Worms')
 
-# Инициируем цикл 'while'
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
 
-while True:                                    # Продолжать до тех пор, пока я не скажу остановиться
-    firstName = random.choice(first)           # .choice - возвращает случайный элемент из непустой последовательности
-    lastName = random.choice(last)
+    # Инициируем цикл 'while'
 
-    print ("\n\n")
-    print (f"{firstName} {lastName}") # N.B. Далее попробовать с f-строкой
-    print("\n\n")
+    while True:                                    # Продолжать до тех пор, пока я не скажу остановиться
+        first_name = random.choice(first)          # .choice - возвращает случайный элемент из непустой последовательности
+        last_name = random.choice(last)
 
-    try_again = input("\n\nПопробовать ещё? (Нажмите Enter либо n, чтобы выйти)\n")
-    if try_again.lower() == "n":
-        break
+        print ("\n\n")
+        print (f"{first_name} {last_name}") # (N.B. Далее попробовать с f-строкой)
+        print("\n\n")
 
-input("\n Нажмите Enter для завершения работы.")
+        try_again = input("\n\nПопробовать ещё? (Нажмите Enter либо n, чтобы выйти)\n")
+        if try_again.lower() == "n":
+            break
+
+    input("\n Нажмите Enter для завершения работы.")
+
+if __name__ == "__main__":
+    
+    main()    
 
